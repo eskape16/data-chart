cac1503c53607d7aa59047cb29b9ec6c846c610b
irina iskrenova <irina_emm@yahoo.es>
2017-11-08 20:42:03 +0100

    add public rendering, add json endpoint for the dataset

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const path = require('path')
 
-app.get('/', function(req, res) {
-  res.send("Hello Data Chart")
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'))
 })
 
-const server = app.listen(3000, function() {
+app.get('/datasets', (req, res) => {
+  res.set('Content-Type', 'application/json')
+  res.send([1,2,3])
+})
+
+const server = app.listen(3000, () => {
   console.log('server running at port: ' + server.address().port)
 })
